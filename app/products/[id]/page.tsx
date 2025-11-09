27259598109cfe924c6f55551a010358785b0340
Amrit1604 <amritjoshi2004@gmail.com>
2025-11-10 02:39:51 +0530

    deletesssswwww

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -211,7 +211,7 @@ const ProductDetails = async ({ params }: Props) => {
           <p className="section-text">Similar Products</p>
 
           <div className="flex flex-wrap gap-10 mt-7 w-full">
-            {similarProducts.map((product) => (
+            {similarProducts.map((product: Product) => (
               <ProductCard key={product._id} product={product} />
             ))}
           </div>
